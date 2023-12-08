218096f008b2f11d5bb8585f8264ecb2f7727282
JennaBrink33 <145591830+JennaBrink33@users.noreply.github.com>
2023-12-08 15:35:10 -0700

    Website Deploy

---
+++ javascript/javascript.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('#header').click(function() {
+        $('#menu').fadeIn();
+    });
+    $('#close-menu').click(function() {
+        $('#menu').fadeOut();
+    });
+});
+
+
+
+
